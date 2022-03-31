ac2d58b101458d03a979709cac0501bc5dc9c485
okadahideya <hideya400@gmail.com>
2022-03-31 22:50:06 +0900

    PHP-チャット

--- join/index.php
+++ join/index.php
@@ -1,3 +1,70 @@
+<?php
+session_start();
+
+$form = [
+    'name' => '',
+    'email' => '',
+    'password' => ''
+];
+$error = [];
+
+/* htmlspcialcharsを短くする */
+function h($value) {
+    return htmlspecialchars($value, ENT_QUOTES);
+}
+
+/* フォームのチェック */
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    if($form['name'] === ''){
+        $error['name'] = 'blank';
+    }
+
+    $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    if($form['email'] === ''){
+        $error['email'] = 'blank';
+    }
+
+    $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    if($form['password'] === ''){
+        $error['password'] = 'blank';
+    }else if(strlen($form['password']) < 4){
+        $error['password'] = 'length';
+    }
+
+    //画像のチェック
+    $image = $_FILES['image'];
+    if($image['name'] !== '' && $image['error'] === 0){
+        $type = mime_content_type($image['tmp_name']);
+        // var_dump($type);
+        if($type !== 'image/png' && $type !== 'image/jpeg'){
+            $error['image'] = 'type';
+        }
+    }
+
+    if(empty($error)){
+        $_SESSION['form'] = $form;
+        
+        //画像のアップロード
+        if($image['name'] !== ''){
+            $filename = date('YmdHis').'_'.$image['name'];
+            If(!move_uploaded_file($image['tmp_name'], '../member_picture/' . $filename)){
+              die('フィイルのアップロードに失敗しました');
+            }
+            $_SESSION['form']['image'] = $filename;
+        }else{
+            $_SESSION['form']['image'] = '';
+        }
+
+        // var_dump($filename);
+        // exit();
+
+        header('Location: check.php');
+        exit();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -22,24 +89,34 @@
             <dl>
                 <dt>ニックネーム<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="name" size="35" maxlength="255" value=""/>
-                    <p class="error">* ニックネームを入力してください</p>
+                    <input type="text" name="name" size="35" maxlength="255" value="<?php echo h($form['name']); ?>"/>
+                    <?php if(isset($error['name']) && $error['name'] === 'blank'): ?>
+                        <p class="error">* ニックネームを入力してください</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>メールアドレス<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="email" size="35" maxlength="255" value=""/>
-                    <p class="error">* メールアドレスを入力してください</p>
-                    <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
+                    <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($form['email']); ?>"/>
+                    <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
+                        <p class="error">* メールアドレスを入力してください</p>
+                    <?php endif; ?>
+                        <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
                 <dt>パスワード<span class="required">必須</span></dt>
                 <dd>
-                    <input type="password" name="password" size="10" maxlength="20" value=""/>
-                    <p class="error">* パスワードを入力してください</p>
-                    <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <input type="password" name="password" size="10" maxlength="20" value="<?php echo h($form['password']); ?>"/>
+                    <?php if(isset($error['password']) && $error['password'] === 'blank'): ?>
+                        <p class="error">* パスワードを入力してください</p>
+                    <?php endif; ?>
+                    <?php if(isset($error['password']) && $error['password'] === 'length'): ?>
+                        <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>写真など</dt>
                 <dd>
                     <input type="file" name="image" size="35" value=""/>
-                    <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                    <?php if(isset($error['image']) && $error['image'] === 'type'): ?>
+                        <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                    <?php endif; ?>
                     <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
                 </dd>
             </dl>
